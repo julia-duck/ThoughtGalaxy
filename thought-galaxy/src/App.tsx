ab9061abc1e7bf59b930d599febc1d5de136f967
Julia Jeng <juliajeng48@gmail.com>
2026-05-24 21:12:00 -0600

    Basic routing implemented

--- thought-galaxy/src/App.tsx
+++ thought-galaxy/src/App.tsx
@@ -3,15 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import {Outlet, Link} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+  /*const [count, setCount] = useState(0) //0 is the count*/
 
   return (
-    <div>
-      <h1>HIII</h1>
+    <div className = "app-container">
+      <nav className = "menu">
+        <Link to="/diary">Diary</Link>
+        <Link to="/to-do">To-Do</Link>
+      </nav>
+      <main>
+        <Outlet />
+      </main> 
     </div>
   )
 }
 
-export default App
+
+/*function Menu() {
+    return
+}*/
+export function Entry() {
+  return <h1 className="app-name">Thought Galaxy</h1>;
+}
+export function Diary() {
+  return <h1>Diary</h1>;
+}
+export function ToDo() {
+  return <h1>To-Do</h1>;
+}
--- thought-galaxy/src/main.tsx
+++ thought-galaxy/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Diary, ToDo, Entry} from "./App.tsx";
+
+const router = createBrowserRouter([
+  { path: '/', 
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Entry />
+      },
+      {
+        path: 'diary',
+        element: <Diary />
+      },
+      {
+        path: 'to-do',
+        element: <ToDo />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router = {router} />
+  </React.StrictMode>
 )
+
+
